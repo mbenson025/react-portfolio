1840fda49dd180f21ac1a6ca426708887324ade6
mbenson025 <mbenson025@gmail.com>
2022-08-28 14:57:38 -0500

    setup contact me form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,6 +7,38 @@ const Contact = () => {
       <div className="container contact-page">
         <div className="text-zone">
           <h1>Contact Me</h1>
+          <p>Send me a message:</p>
+          <div className="contact form">
+            <form>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea name="message" placeholder="Message" required />
+                </li>
+                <li>
+                  <input className="flat-button" type="submit" value="Send" />
+                </li>
+              </ul>
+            </form>
+          </div>
         </div>
       </div>
       <Loader type="line-scale-pulse-out" />
