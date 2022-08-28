84aeb3b5a909136cde5d87d230789ea45e5540c1
mbenson025 <mbenson025@gmail.com>
2022-08-27 19:03:35 -0500

    setup home page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Layout from './components/Layout';
 import './App.scss';
 import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </>
   );
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,9 +1,24 @@
 import './index.scss';
 import NavBar from '../NavBar';
-// import { Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
-  return <NavBar />;
+  return (
+    <div className="App">
+      <NavBar />;
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
+
+        <Outlet />
+
+        <span className="tags bottom-tags">
+          &lt;/body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
