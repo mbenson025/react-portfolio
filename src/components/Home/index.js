f69d40cf34d1405f1289036e98ed687e8dec9435
mbenson025 <mbenson025@gmail.com>
2022-08-28 14:09:52 -0500

    pulse animation on home and about pages

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,19 +1,23 @@
 import { Link } from 'react-router-dom';
 import './index.scss';
 import Logo from './Logo';
+import Loader from 'react-loaders';
 
 const Home = () => {
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>Mason Benson</h1>
-        <h2> Full Stack Developer </h2>
-        <Link to="/contact" className="flat-button">
-          Contact Me
-        </Link>
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>Mason Benson</h1>
+          <h2> Full Stack Developer </h2>
+          <Link to="/contact" className="flat-button">
+            Contact Me
+          </Link>
+        </div>
+        <Logo />
       </div>
-      <Logo />
-    </div>
+      <Loader type="line-scale-pulse-out" />
+    </>
   );
 };
 
