eb95a33393a3572b2c9432294f6a09d9fc72f484
mbenson025 <mbenson025@gmail.com>
2022-08-28 10:43:50 -0500

    changed home logo placement

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,10 +1,10 @@
 import './index.scss';
-import LogoS from '../../../assets/images/mblogo2.png';
+import LogoMB from '../../../assets/images/mblogo2.png';
 
 const Logo = () => {
   return (
     <div className="logo-container">
-      <img className="solid-logo" src={LogoS} alt="S" />
+      <img className="solid-logo" src={LogoMB} alt="MB" />
     </div>
   );
 };
