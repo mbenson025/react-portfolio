e2e81d7565c57c9288cb9f95bf58f045c2938869
mbenson025 <mbenson025@gmail.com>
2022-08-29 13:35:09 -0500

    connecting files to display each project

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Loader from 'react-loaders';
 import './index.scss';
+import ProjectItem from './ProjectData';
 
 const Projects = () => {
   return (
     <>
-      <div></div>
+      <div className="projects">
+        <h1>My Projects</h1>
+        <div classname="projectList">
+          <ProjectItem />
+          <ProjectItem />
+        </div>
+      </div>
       <Loader type="line-scale-pulse-out" />
     </>
   );
