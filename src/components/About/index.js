002e2da548a7502a4a14274108e637239cd4faea
mbenson025 <mbenson025@gmail.com>
2022-08-29 00:08:46 -0500

    color changes and aboutme content

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,8 +9,19 @@ const About = () => {
           <h1>About Me</h1>
 
           <p>
-            I'm a passionate full stack developer from Chicago, IL. In my free
-            time I like to going hiking with my dog.
+            I'm a passionate full stack developer from Chicago, IL. I love
+            learning about technology and spending time with my dog.
+          </p>
+          <p>
+            Background in corporate IT account management where I have
+            experience working with collaboration technologies, IoT, cloud &
+            managed services, data center, and telecommunications.
+          </p>
+          <br></br>
+          <p>
+            <strong>Skills:</strong> JavaScript, ES6, HTML5, CSS3, jQuery, AJAX,
+            Node.js, Express.js, React, MySQL, Sequelize, NoSQL, MongoDB,
+            Mongoose, Postman, Insomnia, RESTful APIs and Server Side APIs
           </p>
         </div>
       </div>
