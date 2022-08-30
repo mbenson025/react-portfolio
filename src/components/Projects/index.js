f6ea9a850b30753f54d83784379e9c543c938a10
mbenson025 <mbenson025@gmail.com>
2022-08-30 08:46:45 -0500

    fixed position sidebar. added scrolling back

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Loader from 'react-loaders';
 import './index.scss';
-import ProjectItem from './ProjectData';
-import ProjectOne from '../../assets/images/logo3.png';
+import ProjectItem from './ProjectItems';
+import { ProjectList } from '../../helpers/ProjectList';
 
 const Projects = () => {
   return (
@@ -10,8 +10,9 @@ const Projects = () => {
       <div className="projects">
         <h1>My Projects</h1>
         <div classname="projectList">
-          <ProjectItem className="techBlog" image={ProjectOne} />
-          <ProjectItem />
+          {ProjectList.map((project) => {
+            return <ProjectItem name={project.name} image={project.image} />;
+          })}
         </div>
       </div>
       <Loader type="line-scale-pulse-out" />
---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,33 @@
+import ProjectOne from '../assets/images/logo3.png';
+import ProjectTwo from '../assets/images/logo3.png';
+import ProjectThree from '../assets/images/logo3.png';
+import ProjectFour from '../assets/images/logo3.png';
+import ProjectFive from '../assets/images/logo3.png';
+import ProjectSix from '../assets/images/logo3.png';
+
+export const ProjectList = [
+  {
+    name: 'MVC Tech Blog',
+    image: ProjectOne,
+  },
+  {
+    name: 'Get Higher',
+    image: ProjectTwo,
+  },
+  {
+    name: 'Weather Dashboard',
+    image: ProjectThree,
+  },
+  {
+    name: 'Employee Tracker',
+    image: ProjectFour,
+  },
+  {
+    name: 'Work Day Scheduler',
+    image: ProjectFive,
+  },
+  {
+    name: 'Take a Hike',
+    image: ProjectSix,
+  },
+];
